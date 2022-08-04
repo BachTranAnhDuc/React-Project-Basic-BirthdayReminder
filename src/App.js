c72e22b09e3642c17853293a03e11676bc24c576
Duc Bach <bachtrananhduc@gmail.com>
2022-08-04 15:10:24 +0700

    front end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import Birth from './Birth';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Birth></Birth>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Birth.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import data from './data.json';
+
+const Birth = () => {
+  return (
+    <>
+      <h2>24 birthdays today</h2>
+      {data.map((el) => {
+        const { id, name, age, image } = el;
+        return (
+          <div key={id} className="birth__detail">
+            <img src={image} alt="img" className="birth__img" />
+            <h5 className="birth__author">{name}</h5>
+            <p className="birth__year">{age} years</p>
+            <button className="btn-remove">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="birth__icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            </button>
+          </div>
+        );
+      })}
+      {/* <div className="birth__detail">
+        <img src="#" alt="img" className="birth__img" />
+        <h5 className="birth__author">Anh Duc</h5>
+        <p className="birth__year">29 years</p>
+      </div>
+      <div className="birth__detail">
+        <img src="#" alt="img" className="birth__img" />
+        <h5 className="birth__author">Anh Duc</h5>
+        <p className="birth__year">29 years</p>
+      </div>
+      <div className="birth__detail">
+        <img src="#" alt="img" className="birth__img" />
+        <h5 className="birth__author">Anh Duc</h5>
+        <p className="birth__year">29 years</p>
+      </div> */}
+      <button className="btn">Clear all</button>
+    </>
+  );
+};
+
+export default Birth;
